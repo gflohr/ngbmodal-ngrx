9c14c89e8d50d8851ae1164d81350edaf1ed10a3
Guido Flohr <guido.flohr@cantanea.com>
2019-12-05 15:15:42 +0200

    working until first cancel

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, exhaustMap } from 'rxjs/operators';
+import { map, exhaustMap, catchError } from 'rxjs/operators';
 
 import * as CounterActions from './counter.actions';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ResetConfirmationComponent } from './reset-confirmation/reset-confirmation.component';
+import { of } from 'rxjs';
 
 @Injectable()
 export class AppEffects {
@@ -22,6 +23,7 @@ export class AppEffects {
 	resetConfirmation$ = createEffect(() => this.actions$.pipe(
 		ofType(CounterActions.resetConfirmation),
 		exhaustMap(() => this.runDialog(ResetConfirmationComponent)),
-		map(() => CounterActions.reset())
+		map(() => CounterActions.reset()),
+		catchError(() => of(CounterActions.resetConfirmationDismiss()))
 	));
 }
